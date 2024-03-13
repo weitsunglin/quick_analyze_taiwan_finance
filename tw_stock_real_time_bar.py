eb82ad106f69ea87a12c05795e82c259da67e607
weitsunglin <wilson.lwt.tw@gmail.com>
2024-03-13 23:46:09 +0800

    Automated commit message

--- tw_stock_real_time_bar.py
+++ tw_stock_real_time_bar.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import os
 from matplotlib.font_manager import FontProperties
 
-
 def scrape_stock_price(stock_code):
     url = f'https://www.cnyes.com/twstock/{stock_code}'
     response = requests.get(url)
@@ -25,13 +24,16 @@ def scrape_stock_price(stock_code):
     else:
         return stock_code, None
 
-stock_codes = ['3293', '2376','2454','2330','2308'] # 可自行增加好孩子
-stock_prices = {code: scrape_stock_price(code)[1] for code in stock_codes if scrape_stock_price(code)[1] is not None}
+original_stock_codes = ['3293', '2376', '2454', '2330', '2308']
+scraped_data = [scrape_stock_price(code) for code in original_stock_codes]
+
+# Filter out None prices and separate codes and prices
+filtered_data = [(code, price) for code, price in scraped_data if price is not None]
+stock_codes, prices = zip(*filtered_data)  # This unpacks the filtered list into two tuples
 
 font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=14)
 
 plt.figure(figsize=(10, 6))
-prices = [price for price in stock_prices.values()]
 
 min_price = min(prices) * 0.9
 max_price = max(prices) * 1.1
@@ -43,16 +45,13 @@ plt.title('台灣小海豹嚴選監控中', fontproperties=font)
 plt.xticks(rotation=45)
 plt.ylim(min_price, max_price)  # 設定y軸範圍
 
-
 for bar in bars:
     yval = bar.get_height()
-    plt.text(bar.get_x() + bar.get_width()/2, yval + (max_price-min_price)*0.01, f'{yval:.2f}', ha='center', va='bottom', fontproperties=font)
-
+    plt.text(bar.get_x() + bar.get_width()/2, yval + (max_price - min_price) * 0.01, f'{yval:.2f}', ha='center', va='bottom', fontproperties=font)
 
 directory = "C:/Users/User/Desktop/project/quick_analyze_stock/"
 if not os.path.exists(directory):
     os.makedirs(directory)
-
 
 save_path = os.path.join(directory, "stock_prices.png")
 plt.savefig(save_path)
