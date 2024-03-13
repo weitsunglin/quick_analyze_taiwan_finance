f6a0de60052a5f5d0f828df8bc6dd0d190875d4c
weitsunglin <wilson.lwt.tw@gmail.com>
2024-03-13 22:44:41 +0800

    Jenkins Automated commit message

--- tw_stock_real_time_bar.py
+++ tw_stock_real_time_bar.py
@@ -1,3 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import os
+from matplotlib.font_manager import FontProperties
+
+
 def scrape_stock_price(stock_code):
     url = f'https://www.cnyes.com/twstock/{stock_code}'
     response = requests.get(url)
@@ -7,37 +14,44 @@ def scrape_stock_price(stock_code):
         if not stock_price:
             stock_price = soup.find('h3', class_='jsx-2312976322 fall')
         if stock_price:
-            # 假設股票價格是文字中的第一個數字，這可能需要根據實際情況調整
             price_text = stock_price.text.split()[0].replace(',', '')
             try:
                 price = float(price_text)
                 return stock_code, price
             except ValueError:
-                # 如果轉換失敗，可能需要進一步檢查文字格式
                 return stock_code, None
         else:
             return stock_code, None
     else:
         return stock_code, None
 
-# 接下來，運行爬蟲並收集價格資料
+
 stock_codes = ['3293', '2376', '2357']
-stock_prices = {code: scrape_stock_price(code)[1] for code in stock_codes}
+stock_prices = {code: scrape_stock_price(code)[1] for code in stock_codes if scrape_stock_price(code)[1] is not None}
 
 
-import matplotlib.pyplot as plt
+font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=14)
 
-# 確保stock_prices字典已經有值
-stock_codes = list(stock_prices.keys())
-prices = [price for price in stock_prices.values() if price is not None]
 
 plt.figure(figsize=(10, 6))
-plt.bar(stock_codes, prices, color='blue')
-plt.xlabel('股票代碼')
-plt.ylabel('價格 (nt)')
-plt.title('股票價格')
-plt.xticks(rotation=45)
+prices = [price for price in stock_prices.values()]
 
-# 儲存圖表
-save_path = "C:/Users/User/Desktop/project/quick_analyze_stock/stock_prices.png"
+
+min_price = min(prices) * 0.9
+max_price = max(prices) * 1.1
+
+plt.bar(stock_codes, prices, color='blue')
+plt.xlabel('股票代號', fontproperties=font)
+plt.ylabel('價格 (nt)', fontproperties=font)
+plt.title('台灣小海豹嚴選監控中', fontproperties=font)
+plt.xticks(rotation=45)
+plt.ylim(min_price, max_price)  # 設定y軸範圍
+
+
+directory = "C:/Users/User/Desktop/project/quick_analyze_stock/"
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+
+save_path = os.path.join(directory, "stock_prices.png")
 plt.savefig(save_path)
