26e05240fef83633c0d2c761a0158e4da83f0230
weitsunglin <wilson.lwt.tw@gmail.com>
2024-03-15 23:54:12 +0800

    Automated commit message

--- test.py
+++ test.py
@@ -3,21 +3,17 @@ from bs4 import BeautifulSoup
 
 url = 'https://www.cnyes.com/twstock/ps_historyprice.aspx?code=2356'
 
-# 發送HTTP請求
 response = requests.get(url)
 
-# 檢查響應狀態碼是否為200（成功）
 if response.status_code == 200:
-    # 使用BeautifulSoup解析HTML內容
     soup = BeautifulSoup(response.text, 'html.parser')
     
-    # 根據網頁結構定位到你想要抓取的數據
-    # 這裡需要你根據實際的HTML結構來定位和提取數據
-    data = soup.find_all('你感興趣的HTML元素或class名稱')
+    # 使用class來找到所有相應的<td>標籤
+    prices = soup.find_all('td', class_='rt')
+    dates = soup.find_all('td', class_='cr')
     
-    # 處理和打印數據
-    for item in data:
-        print(item.text)
+    # 假設價格和日期是一一對應的，我們可以同時遍歷它們
+    for price, date in zip(prices, dates):
+        print(f'Date: {date.text}, Price: {price.text}')
 else:
     print("Failed to retrieve data")
-
