376452f8e6ea73b5bbf9a70a187dfb31a43a3eaf
weitsunglin <wilson.lwt.tw@gmail.com>
2024-03-16 22:03:42 +0800

    Automated commit message

--- top15_stocks_trade_value.py
+++ top15_stocks_trade_value.py
@@ -11,12 +11,12 @@ if response.status_code == 200:
     data = response.json()
     df = pd.DataFrame(data)
     df['TradeValue'] = pd.to_numeric(df['TradeValue'], errors='coerce')
-    top10 = df.sort_values(by='TradeValue', ascending=False).head(10)
+    top10 = df.sort_values(by='TradeValue', ascending=False).head(15)
 
-    # 设置支持繁体中文的字体
-    font_path = "C:\\Windows\\Fonts\\msjh.ttc"  # 字体路径
+    # 繁中
+    font_path = "C:\\Windows\\Fonts\\msjh.ttc"
     font_properties = FontProperties(fname=font_path, size=12)
-    plt.rcParams['axes.unicode_minus'] = False  # 正确显示负号
+    plt.rcParams['axes.unicode_minus'] = False 
 
     plt.style.use('ggplot')
     ax = top10.plot(kind='bar', x='Name', y='TradeValue', legend=None)
@@ -27,9 +27,7 @@ if response.status_code == 200:
     
     plt.tight_layout()
 
-    # 保存图表到本地路径
-    save_path = 'C:\\Users\\User\\Desktop\\project\\quick_analyze_stock\\top10_stocks_trade_value.png'
+    save_path = 'C:\\Users\\User\\Desktop\\project\\quick_analyze_stock\\top15_stocks_trade_value.png'
     plt.savefig(save_path)
-    plt.show()
 else:
     print("Failed to retrieve data, status code:", response.status_code)
